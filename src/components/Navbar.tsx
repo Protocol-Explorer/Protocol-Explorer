3e3e178fb2968cf826e5f6cfdda9fd39fcbf19e4
razacodespython <srh.zaidi@outlook.com>
2023-09-26 16:36:31 +0200

    landing page, call out

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+// components/Navbar.js
+
+import React from "react";
+import Link from "next/link";
+import styles from "@/styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/exercises">Train!</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/blog">Blog</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
