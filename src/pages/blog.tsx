ddb82a0bbbd76f0cd12c96f1ad1a5456db2a8bef
Rahat <rahat@rahatcodes.com>
2023-09-26 22:41:25 -0400

    add nav everywhere

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from '@/styles/blog.module.css'
+import TelegramCall from "@/components/TelegramCall";
+import Navbar from "@/components/Navbar";
 
 const Blog = () => {
   return (
+    <>
+    <TelegramCall />
+      <Navbar />
     <div className={styles.container}>
         <h1 className={styles.text}>Coming Soon</h1>
     </div>
+    </>
 );
 }
 
