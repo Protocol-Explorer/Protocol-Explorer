fa5c22ef5684c70a3506a744d14ef754b7d688f7
Rahat <rahat@rahatcodes.com>
2023-09-29 09:26:17 -0400

    fix lintint breakage

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -15,6 +15,7 @@ const About = () => {
       <div className={styles.cardContainer}>
         {/* Card 1 */}
         <div className={styles.card}>
+          {/* eslint-disable-next-line @next/next/no-img-element*/}
           <img src="/rahat.png" alt="Rahat" className={styles.image}/>
           <h2 className={styles.name}>Rahat</h2>
           <p className={styles.blurb}>Musician turned coder turned teacher turned something else. Just building and and helping others build along the way. Take me very seriously.</p>
@@ -26,9 +27,10 @@ const About = () => {
         
         {/* Card 2 */}
         <div className={styles.card}>
+           {/* eslint-disable-next-line @next/next/no-img-element*/}
           <img src="/raza.png" alt="Raza" className={styles.image}/>
           <h2 className={styles.name}>Raza</h2>
-          <p className={styles.blurb}>A coding nerd voyaging into tech. I try to learn and explain complicated shit. I spent most of my time building and breaking stuff. Don't take me too seriously.</p>
+          <p className={styles.blurb}>A coding nerd voyaging into tech. I try to learn and explain complicated shit. I spent most of my time building and breaking stuff. Don&apos;t take me too seriously.</p>
           <div className={styles.links}>
             <a href="https://www.razacodes.com/" className={styles.link}>Website</a>
             <a href="https://twitter.com/razacodes" className={styles.link}>Twitter</a>
