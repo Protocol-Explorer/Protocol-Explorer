6274ecc107fca6c536a2d42fdaba00e5826d156e
Shiv2116 <shivdixit61@gmail.com>
2024-04-13 13:14:07 +0530

    Revert "changes of explore page"
    This reverts commit 186003d04dfb17eb866da14a4aba7ed08f5d2210.

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,14 +1,14 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import TelegramCall from "@/components/TelegramCall";
-import styles from "@/styles/explore.module.css";
+import styles from "@/styles/explore.module.css"
 import ProtocolCard from "@/components/ProtocolCard";
-import { useState } from "react";
-import MultiSelect from "@/components/MultiSelect";
-import Image from "next/image";
+import { useState } from 'react';
+import MultiSelect from '@/components/MultiSelect';
+
 interface Protocol {
   name: string;
   website: string;
@@ -26,8 +26,8 @@ interface ProtocolPageProps {
   protocols: Protocol[];
 }
 
-export default function Explore({ protocols }: ProtocolPageProps) {
-  const [protocolData, setProtocolData] = useState(protocols);
+export default function Explore({ protocols }:ProtocolPageProps) {
+  const [protocolData, setProtocolData] = useState(protocols)
   return (
     <>
       <Head>
@@ -38,46 +38,35 @@ export default function Explore({ protocols }: ProtocolPageProps) {
       </Head>
       <TelegramCall />
       <Navbar />
-      <div className={styles.centerImg}>
-      <Image
-        className={styles.circleSvg}
-        src="/jutsu.jpg"
-        alt="Jutsu"
-        width={100}
-        height={100}
-      />
-      </div>
       <h1 className={styles.header}>Explore the World of Web3</h1>
       <MultiSelect protocols={protocols} setProtocolData={setProtocolData} />
       <main className={styles.main}>
-        {protocolData.map((protocol, i) => (
-          <ProtocolCard key={i} {...protocol} />
-        ))}
+        {protocolData.map((protocol, i) => <ProtocolCard key={i} {...protocol} />)}
       </main>
     </>
   );
 }
 
 export const getStaticProps = async () => {
-  const files = fs.readdirSync(path.join("src", "protocols"));
+  const files = fs.readdirSync(path.join('src', 'protocols'));
 
   const protocols = files.map((filename) => {
-    const markdownWithMeta = fs.readFileSync(
-      path.join("src", "protocols", filename),
-      "utf-8"
-    );
+      const markdownWithMeta = fs.readFileSync(
+          path.join('src', 'protocols', filename),
+          'utf-8'
+      );
 
-    const { data: frontMatter } = matter(markdownWithMeta);
+      const { data: frontMatter } = matter(markdownWithMeta);
 
-    return {
-      slug: filename.replace(".mdx", ""),
-      ...frontMatter,
-    };
+      return {
+          slug: filename.replace('.mdx', ''),
+          ...frontMatter,
+      };
   });
 
   return {
-    props: {
-      protocols,
-    },
+      props: {
+          protocols,
+      },
   };
 };
