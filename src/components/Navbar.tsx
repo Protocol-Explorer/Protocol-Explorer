e69b6b37abe55e930964430cde417b7a4da8d26c
Shiv2116 <shivdixit61@gmail.com>
2024-05-05 15:46:54 +0530

    navbar mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,43 @@
 // components/Navbar.js
 
-import React from "react";
+import React,{useState} from "react";
 import Link from "next/link";
 import styles from "@/styles/Navbar.module.css";
 import Image from "next/image";
 import { WrappedBuildError } from "next/dist/server/base-server";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
   const links = [
     { url: 'https://twitter.com/protocolxplorer', imgPath: '/jutsu.jpg' },
     { url: 'https://twitter.com/Rahatcodes', imgPath: '/rahat.png' },
     { url: 'https://twitter.com/razacodes', imgPath: '/raza.png' },
   ];
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+};
   return (
     <nav className={styles.navbar}>
+      <div className={styles.navbarContainer} >
       <Link href="/">
         <div className={styles.frame}>
           <div className={styles.grupo}>
             <img className={styles.path} alt="Path" src="jutsulogo.png" />
             <div className={styles.overlapGroupWrapper}>
-              <div className={styles.overlapGroup}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={80}
-                  height={36}
-                  viewBox="0 0 106 36"
-                  fill="none"
-                  x="0"
-                >
-                  <path
-                    d="M18.0001 -1.52588e-05H87.0001C91.774 -1.52588e-05 96.3523 1.89641 99.728 5.27206C103.104 8.64772 105 13.2261 105 18C105 22.7739 103.104 27.3523 99.728 30.7279C96.3523 34.1036 91.774 36 87.0001 36H6.10352e-05V18C6.10352e-05 13.2261 1.89648 8.64772 5.27214 5.27206C8.64779 1.89641 13.2262 -1.52588e-05 18.0001 -1.52588e-05Z"
-                    fill="#E4E4F9"
-                  />
-                </svg>
                 <div className={styles.textWrapper}>Explorer</div>
-              </div>
             </div>
-            <img className={styles.caminho} alt="Caminho" src="/Caminho.png" />
+            <span className={styles.caminho}>Protocol</span>
           </div>
         </div>
       </Link>
-
-      <div className={styles.navLinks}>
+      <button className={styles.burgerButton} onClick={toggleMenu}>
+        <span className={styles.span}></span>
+        <span className={styles.span}></span>
+        <span className={styles.span}></span>
+      </button>
+      </div>
+      <div className={`${styles.navLinks} ${isOpen ? styles.showNav:""}`}>
         <Link href="/explore" className={styles.navLink}>
           Explore
         </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -124,6 +124,7 @@ export default function Home() {
         </div>
       </div>
       <Features />
+      
     </>
   ); 
 }
